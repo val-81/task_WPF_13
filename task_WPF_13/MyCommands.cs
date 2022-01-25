ad397e4784071016f8d647a5316dd7e8435d5616
Валентин Ковальчук <val-81@mail.ru>
2022-01-25 21:33:53 +0300

    Добавьте файлы проекта.

---
+++ task_WPF_13/MyCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace task_WPF_13
+{
+    class MyCommands
+    {
+        public static RoutedCommand Exit { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            Exit = new RoutedCommand("Exit", typeof(MyCommands), inputs);
+        }
+    }
+}
